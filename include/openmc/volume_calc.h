24c50eaf2199565fc7abfa2354d2816cfbc5f4d3
Will Boyd <wbinventor@gmail.com>
2018-10-06 14:44:25 -0400

    Started port of volume calculation from Fortran to C++

--- include/openmc/volume_calc.h
+++ include/openmc/volume_calc.h
@@ -6,45 +6,57 @@
 #include <map>
 
 #ifdef _OPENMP
-  #incude <omp.h>
+  #include <omp.h>
 #endif
 
-#include <openmc/constants.h>
-#include <openmc/xml_interface.h>
+#include "openmc/constants.h" // FILTER_CELL, FILTER_MATERIAL, FILTER_UNIVERSE
+#include "openmc/xml_interface.h"  // ???
+#include "openmc/error.h"  // fatal_error(...), write_message(...)
+#include "pugixml.hpp"  // XML
+#include "openmc/geometry.h" // find_cell(...)
+#include "openmc/hdf5_interface.h" // file_open, ...
+#include "openmc/material.h" // std::vector<Material*> materials
+#include "openmc/message_passing.h"
+// nuclide_header ???
+#include "openmc/random_lcg.h" // prn, prn_set_stream, set_particle_seed
+#include "openmc/settings.h" // path_output
+#include "openmc/capi.h"  // openmc_get_volumes() declaration
+// timer_header ???
 
 
 class VolumeCalculation {
-private:
-    int domain_type;
-    std::vector<int> domain_id;
-    double lower_left[3];
-    double upper_right[3];
-    int samples;
-    std::map<int, std::vector<int> > hits;
-
-    std::vector<int> volumes;
-    std::vector<int> nuclide_vec;
-    std::vector<double> atoms_vec;
-    std::vector<double> uncertainty_vec;
-
-    // FIXME: This may ned to take more parameters...
-    void check_hit(int domain, int material);
-
 public:
-    void calculate_volume();
-    void write_volume(std::string filename, double volume,
-                      std::vector<int> nuclide_vec,
-                      std::vector<double> atoms_vec,
-                      std::vector<double> uncertainty_vec);
-    void from_xml(xml_node_struct* xml_node);
-}
+  // Constructors
+  VolumeCalculation() = default;
+  VolumeCalculation(pugi::xml_node node);
 
+//  virtual ~VolumeCalculation();  // Needed??
+  int domain_type;
+  std::vector<int> domain_ids;
+  std::vector<double> lower_left;
+  std::vector<double> upper_right;
+  int n_samples;
 
-// FIXME: Should I make this a static class method for VolumeCalculation?
-VolumeCalculation* volume_from_xml(xml_node_struct* xml_node);
+  /* ???
+  std::map<int, std::vector<int> > hits;
+  std::vector<int> volumes;
+  std::vector<int> nuclide_vec;
+  std::vector<double> atoms_vec;
+  std::vector<double> uncertainty_vec;
+  */
 
-void openmc_calculate_volumes();
+  // FIXME: This may ned to take more parameters...
+  void check_hit(int domain, int material);
 
-// FIXME:  Make volume_cals (collection of VolumeCalculation) a global variable???
+  void set_domain_type(int domain_type);
+  void calculate_volumes(
+      openmc::double_2dvec volumes, std::vector<int> i_nuclides,
+      std::vector<double> n_atoms, std::vector<double> n_atoms_uncertainty);
+  void write_volume(
+    std::string filename, openmc::double_2dvec volumes, std::vector<int> i_nuclides,
+    std::vector<double> n_atoms, std::vector<double> n_atoms_uncertainty);
+
+};
+
 
 #endif // VOLUME_CALC_H
--- src/volume_calc.cpp
+++ src/volume_calc.cpp
@@ -1,45 +1,152 @@
-#include <openmc/volume_calc.h>
+#include "openmc/volume_calc.h"
+
+//==============================================================================
+// Global variables
+//==============================================================================
+
+std::vector<VolumeCalculation> volume_calcs;
 
 
-void VolumeCalculation::calculate_volume() {
+//==============================================================================
+// VolumeCalculation implementation
+//==============================================================================
 
-}
+VolumeCalculation::VolumeCalculation(pugi::xml_node node) {
 
-void VolumeCalculation::write_volume(std::string filename, double volume,
-                                     std::vector<int> nuclide_vec,
-                                     std::vector<double> atoms_vec,
-                                     std::vector<double> uncertainty_vec) {
+  // NOTE: get_node_value(...) takes optional bool args lowercase and strip (False by default)
 
-}
-
-void VolumeCalculation::check_hit(int domain, int material) {
-
-}
-
-
-void VolumeCalculation::from_xml(xml_node_struct* xml_node) {
-  // TODO: Look at XML CPP API...
-
-  // FIXME: Check domain type (cell, material or universe)
-  std::string domain_type = get_node_value(xml_node, "domain_type");
-
+  // Read domain type (cell, material or universe)
+  std::string domain_type = openmc::get_node_value(node, "domain_type");
   if (domain_type == "cell") {
-    this.domain_type = FILTER_CELL;
+    this->domain_type = openmc::FILTER_CELL;
   }
   else if (domain_type == "material") {
-    this->domain_type = FILTER_MATERIAL;
+    this->domain_type = openmc::FILTER_MATERIAL;
   }
   else if (domain_type == "universe") {
-    this->domain_type = FILTER_UNIVERSE;
+    this->domain_type = openmc::FILTER_UNIVERSE;
   }
   else {
-    std::stringstream err_msg;
-    err_msg << "Unrecognized domain type for stochastic volume "
-            << "calculation: \"" << domain_type << "\"";
-    fatal_error(err_msg);
+    openmc::fatal_error(std::string("Unrecognized domain type for stochastic "
+           "volume calculation:" + domain_type));
   }
+
+  // Read domain IDs, bounding corodinates and number of samples
+  this->domain_ids = openmc::get_node_array<int>(node, "domain_ids");
+  this->lower_left = openmc::get_node_array<double>(node, "lower_left");
+  this->upper_right = openmc::get_node_array<double>(node, "upper_right");
+  this->n_samples = std::stoi(openmc::get_node_value(node, "samples"));
 }
 
-void openmc_calculate_volumes() {
+//VolumeCalculation::~VolumeCalculation() {
+  // FIXME: deallocate memory for arrays; i.e., volume_calcs
+//}
 
+
+  // FIXME: This may ned to take more parameters...
+void VolumeCalculation::check_hit(int domain, int material) {
+  return;
 }
+
+
+void VolumeCalculation::calculate_volumes(
+    openmc::double_2dvec volumes, std::vector<int> i_nuclides,
+    std::vector<double> n_atoms, std::vector<double> n_atoms_uncertainty) {
+  return;
+}
+
+void VolumeCalculation::write_volume(
+  std::string filename, openmc::double_2dvec volumes, std::vector<int> i_nuclides,
+  std::vector<double> n_atoms, std::vector<double> n_atoms_uncertainty) {
+  return;
+}
+
+
+//==============================================================================
+// OPENMC_CALCULATE_VOLUMES runs each of the stochastic volume calculations
+// that the user has specified and writes results to HDF5 files
+//==============================================================================
+
+int openmc_calculate_volumes() {
+
+  int err = 0;
+  int i, j, n_domains;
+  openmc::double_2dvec volumes;  // volume mean/stdev in each domain
+  std::string domain_type;
+  std::string filename;    // filename for HDF5 file
+  // FIXME: Initialize a timer...but is there one for C++ yet?
+  std::vector<int> i_nuclides;   // indices in nuclides array
+  std::vector<double> n_atoms;   // total # of atoms of each nuclide
+  std::vector<double> n_atoms_uncertainty;  // uncertainty of total # of atoms
+
+  // FIXME: Does this work for MPI in C++???
+  if (openmc::mpi::master) {
+    openmc::write_message("STOCHASTIC VOLUME CALCULATION", 3);
+    // FIXME: Start the timer here....
+  }
+
+  // FIXME: Where is "volume_calcs" coming from???
+  for (int i = 0; i < volume_calcs.size(); i++) {
+    n_domains = volume_calcs[i].domain_ids.size();
+    i_nuclides.resize(n_domains);
+    n_atoms.resize(n_domains);
+    n_atoms_uncertainty.resize(n_domains);
+
+    // FIXME: This probably won't work for a nested std::vector
+    volumes.resize(2);
+    volumes[0].resize(n_domains);
+    volumes[1].resize(n_domains);
+
+    if (openmc::mpi::master) {
+      std::stringstream msg;
+      msg << "Running volume calculation " << i << "...";
+      openmc::write_message(msg, 4);
+    }
+
+    // TODO: Implement this such that each MPI proc separatel computes the volume
+    // and this function then reduces them at the end
+    volume_calcs[i].calculate_volumes(
+      volumes, i_nuclides, n_atoms, n_atoms_uncertainty);
+
+    // TODO: Implement MPI reduction across volumes, atoms and nuclides
+
+    if (openmc::mpi::master) {
+      if (volume_calcs[i].domain_type == openmc::FILTER_CELL) {
+        domain_type = "  Cell";
+      }
+      else if (volume_calcs[i].domain_type == openmc::FILTER_MATERIAL) {
+        domain_type = "  Material";
+      }
+      else {
+        domain_type = "  Universe";
+      }
+
+      // Display domain volumes
+      for (int j = 0; j < volume_calcs[i].domain_ids.size(); j++) {
+        std::stringstream msg;
+        msg << domain_type << " " << volume_calcs[j].domain_ids[j]
+            << volumes[0][j] << " +/- " << volumes[0][j] << " cm^3";
+        openmc::write_message(msg.str(), 4);
+      }
+    
+      // Write volumes to HDF5 file
+      // FIXME: Can VolumeCalculation method take path and filename as args?
+      // FIXME: Should this be a static class method, since it's only called by the master proc?
+      std::stringstream filename;
+      filename << openmc::settings::path_output << "volume_" << i << ".h5";
+      volume_calcs[i].write_volume(
+        filename.str(), volumes, i_nuclides, n_atoms, n_atoms_uncertainty);
+
+    }
+
+    // FIXME: Need to deallocate any std::vectors???
+
+    // FIXME: Show elapsed time; is there a C++ timer yet???
+  }
+
+  err = 0;
+  return err;
+}
+
+
+
